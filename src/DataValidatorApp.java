30b4c9e06bdd8e8a845f5f0cb381b208d92ef926
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-02-13 09:57:42 +0100

    #1 walidacja pesela, final

---
+++ src/DataValidatorApp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DataValidatorApp {
+    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean valid;
+        do {
+            System.out.println("Pesel: ");
+            String pesel = scanner.nextLine();
+
+            System.out.println("Birth year: ");
+            int birthYear = scanner.nextInt();
+            scanner.nextLine();
+
+            valid = DataValidator.birthDate(pesel, birthYear);
+
+        } while (!valid); // pętla działa tak długo, jak jest niepoprawne.
+    }
+}
